ff569f486397328e46a270fd718382c2a96fa2fe
Yuval <yuvalkat@gmail.com>
2026-01-27 15:42:51 +0200

    add routing for Home, Create, and Post components

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,18 +1,26 @@
 import AppLayout from "@/functions/layout/AppLayout";
-import Login from "@/functions/pages/Login";
-import Profile from "@/functions/pages/Profile";
-import Register from "@/functions/pages/Register";
-import { Routes, Route } from "react-router-dom";
+import Create from "@/functions/posts/pages/Create";
+import Home from "@/functions/posts/pages/Home";
+import Login from "@/functions/auth/pages/Login";
+import Post from "@/functions/posts/pages/Post";
+import Profile from "@/functions/profile/pages/Profile";
+import Register from "@/functions/auth/pages/Register";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function AppRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/home" replace />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
 
       <Route element={<AppLayout />}>
         <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/post/:postId" element={<Post />} />
       </Route>
+      <Route path="*" element={<Navigate to="/home" replace />} />
     </Routes>
   );
 }
