4773f06772231ad1062f0249bf89e7aafa70ea81
dortamir <dortamir291@gmail.com>
2026-01-29 13:49:08 +0200

    render header only for authenticated users

--- src/functions/layout/AppLayout.tsx
+++ src/functions/layout/AppLayout.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Footer as HeaderNav } from "@/functions/layout/Footer";
+import { useSelector } from "react-redux";
+import type { RootState } from "@/services";
 
 export default function AppLayout() {
+  const isUserLoggedIn = useSelector(
+    (state: RootState) => state.auth.isUserLoggedIn
+  );
+
   return (
     <>
+      {isUserLoggedIn && <HeaderNav />}
       <HeaderNav />
-      <main className="pt-16">
+      <main className={isUserLoggedIn ? "pt-16" : "w-full"}>
         <Outlet />
       </main>
     </>
