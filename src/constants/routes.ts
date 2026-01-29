e599212d2e4998135f5d5a044bed4aa5b1d6e19f
dortamir <dortamir291@gmail.com>
2026-01-29 13:14:31 +0200

    add app and routes constants

---
+++ src/constants/routes.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  HOME: "/home",
+  LOGIN: "/login",
+  REGISTER: "/register",
+  PROFILE: "/profile",
+  CREATE_POST: "/create",
+  VIEW_POST: (postId: string) => `/post/${postId}`,
+} as const;
