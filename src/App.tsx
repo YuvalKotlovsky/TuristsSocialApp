2693e28dc76d2dcba4dfcdda528e6d6517348932
dortamir <dortamir291@gmail.com>
2026-01-29 13:31:24 +0200

    remove Header(Footer) from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/Routes";
-import { Footer } from "./functions/layout/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex min-h-screen flex-col items-center justify-center">
         <AppRoutes />
-        <Footer />
       </div>
     </BrowserRouter>
   );
